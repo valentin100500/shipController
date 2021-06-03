82ef5540f585972373656b7707a26507c94b4ec0
valentin100500 <valentinpa100500@gmail.com>
2021-06-03 10:16:42 +0300

    added  `ssid` and `password` of Wi-Fi

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 #include "ShipAPI/ShipAPI.h"
 #include "IDs.h"
 
+const char *ssid = "shipController";
+const char *password = "";
+
 Motor motor = Motor(ID_MAIN_MOTOR, 19);
 Helm helm = Helm(ID_MAIN_HELM, 18);
 
@@ -15,7 +18,7 @@ GetChangeLevel chLevel = GetChangeLevel(ID_CHARGE_LEVEL, 34);
 void setup()
 {
   Serial.begin(115200);
-  WiFiInit(); //Ініціалізація Wi-fi AP
+  WiFiInit(WIFI_AP, ssid, password); //Ініціалізація Wi-fi AP
 
   shipAPI.controller.use(&helm);
   shipAPI.controller.use(&motor);
